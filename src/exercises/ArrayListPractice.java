cd50269d98636e1f7da4e1a1bc9b85054280f16b
LFallert <lfallert13@yahoo.com>
2021-07-21 19:01:26 -0500

    fixed array list

--- src/exercises/ArrayListPractice.java
+++ src/exercises/ArrayListPractice.java
@@ -1,6 +1,7 @@
 package exercises;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -9,27 +10,13 @@ public class ArrayListPractice {
 
     public static void main(String[] args) {
 
-        ArrayList<Integer> numArray = new ArrayList<>();
-        numArray.add(1);
-        numArray.add(1);
-        numArray.add(2);
-        numArray.add(3);
-        numArray.add(5);
-        numArray.add(8);
-        numArray.add(8);
-        numArray.add(11);
-        numArray.add(12);
-        numArray.add(15);
+        ArrayList<Integer> numArray = new ArrayList<>(Arrays.asList(1,1,2,3,5,8,8,11,12,15));
 
         System.out.println(Sum.getSum(numArray));
         Scanner input = new Scanner(System.in);
 
-        ArrayList<String> wordArray = new ArrayList<>();
-        wordArray.add("cat");
-        wordArray.add("zebra");
-        wordArray.add("dog");
-        wordArray.add("snake");
-        wordArray.add("bear");
+        ArrayList<String> wordArray = new ArrayList<>(Arrays.asList("cat", "zebra", "dog", "snake", "bear"));
+        
 
         System.out.println(FiveLetters.hasFiveLetters(wordArray));
 
